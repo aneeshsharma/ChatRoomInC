698989d16eb2c4d4148ccfa149be32d3d258e838
Anish Sharma <aneeshsharma15@outlook.com>
2021-02-12 16:24:36 +0530

    Implemented message streaming to clients

--- client.c
+++ client.c
@@ -3,22 +3,75 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #define PORT 5000
+#define BUFFER_SIZE 1024
+
+void *listen_messages(void *arg)
+{
+    int sock_fd = 0, recv_len;
+    char *name = (char *)arg;
+    struct sockaddr_in server_address;
+    char message[256];
+    char buffer[BUFFER_SIZE];
+
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Error creating socket!!\n");
+        return NULL;
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0)
+    {
+        printf("Invalid address\\Address not supported\n");
+        return NULL;
+    }
+
+    printf("Connecting to message stream...\n");
+
+    if (connect(sock_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    {
+        printf("Connection failed!\n");
+        return NULL;
+    }
+
+    send(sock_fd, name, strlen(name), 0);
+    sleep(1);
+    send(sock_fd, "RECV", strlen("RECV"), 0);
+
+    while (1)
+    {
+        recv_len = recv(sock_fd, buffer, BUFFER_SIZE, 0);
+        if (recv_len == 0)
+        {
+            close(sock_fd);
+            return NULL;
+        }
+        buffer[recv_len] = '\0';
+        printf("%s\n", buffer);
+        fflush(stdout);
+    }
+}
 
 int main()
 {
-    int sock = 0, recv_len;
+    int sock_fd = 0, recv_len;
     struct sockaddr_in server_address;
     char message[256];
     char buffer[1024] = {0};
     char name[20];
 
+    pthread_t listener;
+
     printf("Enter name - ");
     scanf("%[^\n]%*c", name);
     printf("\n");
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("Error creating socket!!\n");
         return -1;
@@ -35,24 +88,27 @@ int main()
 
     printf("Connecting...\n");
 
-    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
+    if (connect(sock_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
     {
         printf("Connection failed!\n");
         return -1;
     }
 
-    send(sock, name, strlen(name), 0);
+    pthread_create(&listener, NULL, listen_messages, name);
+
+    send(sock_fd, name, strlen(name), 0);
+    sleep(1);
+    send(sock_fd, "SEND", strlen("SEND"), 0);
 
     printf("Connected to server\nUse /exit to exit\n");
 
     while (1)
     {
-        printf("> ");
         scanf("%[^\n]%*c", message);
-        send(sock, message, strlen(message), 0);
+        send(sock_fd, message, strlen(message), 0);
         if (strcmp(message, "/exit") == 0)
             break;
     }
-    close(sock);
+    close(sock_fd);
     return 0;
 }
--- server.c
+++ server.c
@@ -15,6 +15,12 @@ typedef struct Client
 {
     int sock_fd;
     char name[20];
+    char message[BUFFER_SIZE];
+    int status;
+    char mode[5];
+    struct Connection *connections;
+    int *num;
+    pthread_mutex_t mut;
 } Client;
 
 typedef struct Connection
@@ -23,29 +29,108 @@ typedef struct Connection
     Client *client;
 } Connection;
 
+void broadcast(char *message, Connection *conn, int num)
+{
+    printf("\033[1;31mNew message - %s\033[0m\n", message);
+    for (int i = 0; i < num; i++)
+    {
+        Client *other = (conn[i]).client;
+
+        pthread_mutex_lock(&(other->mut));
+        if (strcmp(other->mode, "RECV") == 0)
+        {
+            strcpy(other->message, message);
+            other->status = 1;
+        }
+        pthread_mutex_unlock(&(other->mut));
+    }
+}
+
 void *handle_client(void *vargp)
 {
     int recv_len;
     char buffer[BUFFER_SIZE];
+    char mode[5];
+
+    char message[BUFFER_SIZE];
+
     Client *client = (Client *)vargp;
     recv_len = read(client->sock_fd, buffer, BUFFER_SIZE);
+    buffer[recv_len] = '\0';
     strcpy(client->name, buffer);
 
-    printf("%s joined the chat\n", client->name);
+    recv_len = read(client->sock_fd, buffer, BUFFER_SIZE);
+    buffer[recv_len] = '\0';
 
-    while (1)
+    strcpy(mode, buffer);
+    strcpy(client->mode, mode);
+    printf("User %s connected with mode - %s\n", client->name, client->mode);
+    if (strcmp(mode, "SEND") == 0)
     {
-        recv_len = read(client->sock_fd, buffer, BUFFER_SIZE);
-        buffer[recv_len] = '\0';
+        sprintf(message, "%s joined the chat", client->name);
+        broadcast(message, client->connections, *(client->num));
 
-        if (strcmp(buffer, "/exit") == 0)
+        while (1)
         {
-            break;
+            recv_len = read(client->sock_fd, buffer, BUFFER_SIZE);
+            buffer[recv_len] = '\0';
+
+            if (recv_len == 0)
+            {
+                break;
+            }
+
+            if (strcmp(buffer, "/exit") == 0)
+            {
+                break;
+            }
+            sprintf(message, "%s: %s", client->name, buffer);
+
+            broadcast(message, client->connections, *(client->num));
         }
-        printf("%s: %s\n", client->name, buffer);
+
+        Client *other;
+        for (int i = 0; i < *(client->num); i++)
+        {
+            Connection *conn = client->connections;
+            other = conn[i].client;
+            pthread_mutex_lock(&(other->mut));
+            if (other != client)
+            {
+                if (strcmp(other->name, client->name) == 0)
+                {
+                    other->status = 2;
+                }
+            }
+            pthread_mutex_unlock(&(other->mut));
+        }
+
+        sprintf(message, "%s left the chat", client->name);
+        broadcast(message, client->connections, *(client->num));
+    }
+    else if (strcmp(mode, "RECV") == 0)
+    {
+        while (1)
+        {
+            while (1)
+            {
+                pthread_mutex_lock(&(client->mut));
+                if (client->status == 1 || client->status == 2)
+                    break;
+                pthread_mutex_unlock(&(client->mut));
+            }
+            if (client->status == 2)
+            {
+                pthread_mutex_unlock(&(client->mut));
+                break;
+            }
+            send(client->sock_fd, client->message, strlen(client->message), 0);
+            client->status = 0;
+            pthread_mutex_unlock(&(client->mut));
+        }
     }
 
-    printf("%s left chat\n", client->name);
+    printf("Closing connection for %s with mode %s\n", client->name, client->mode);
 
     close(client->sock_fd);
 }
@@ -57,6 +142,8 @@ int main()
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
+
+    int seq = 0;
 
     Connection connections[MAX_CONN];
     int curr = 0;
@@ -93,14 +180,32 @@ int main()
     {
         if ((conn_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
+            printf("Error accepting connection!!!\n");
+            fflush(stdout);
             perror("accept");
-            exit(EXIT_FAILURE);
         }
 
         Client *client = (Client *)malloc(sizeof(Client));
 
         client->sock_fd = conn_socket;
-        pthread_create(&connections[curr].id, NULL, handle_client, (void *)client);
+        client->status = 0;
+        client->connections = connections;
+        connections[curr].client = client;
+
+        if (pthread_mutex_init(&(client->mut), NULL) != 0)
+        {
+            printf("Mutex initialization failed!!\n");
+        }
+        else
+        {
+            pthread_create(&connections[curr].id, NULL, handle_client, (void *)client);
+            printf("New thread created - %d\n", connections[curr].id);
+            curr++;
+            client->num = &curr;
+            printf("User count - %d\n", curr);
+        }
     }
+
+    printf("Loop broken\n");
     return 0;
 }
